25054b9a7bce40635651044b9b262291b20707d9
dev-augustin <dv.augustin00@gmail.com>
2020-11-17 23:03:19 -0600

    learned string index, substring, upper, lower

---
+++ Programs/StrSlice.py
@@ -0,0 +1,17 @@
+# color = 'Orange'
+# print(color[1:4])
+
+# insect = "butterfly"
+# print(insect[:3])
+# print(insect[3:])
+
+## index
+# pets = "Cats & Dogs"
+# print(pets.index("&"))
+# pets.index("D")
+# print("Dragons" in pets)
+# print("Cats" in pets)
+
+############### upper & lowers
+print("flowers".upper())
+print("flowers".lower())
